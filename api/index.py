5c2f1bd6a239957c242745bd2d76f64168339fd3
haigol <hkgolberg@gmail.com>
2025-03-02 14:53:13 +0100

    added hmaxByCoords-endpoint

--- api/index.py
+++ api/index.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import xarray as xr
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://map-waves-assignment-haigols-projects.vercel.app", "https://map-waves-assignment.vercel.app", "https://map-waves-assignment-git-main-haigols-projects.vercel.app"]}})
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET'])
+def home():
+    return "Flask running on port 8080"
+
+
+@app.route('/hmaxByCoords', methods=['POST'])
+def getHMaxAtCoords():
+    data = request.get_json()
+
+    lat = data['lat']
+    lon = data['lon']
+    
+    try:
+        ds = xr.open_dataset("./waves_2019-01-01.nc", engine="netcdf4")
+        ds_hmax_array = ds["hmax"].sel(longitude=lon,latitude=lat, method="nearest")
+        ds_hmax_max_value = (ds_hmax_array.values.max())
+    
+        return str(ds_hmax_max_value) # either string value or nan if no value found (handle on client side)
+    except Exception as e:
+        print("Error occured", e)
+
